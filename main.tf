599e25f2a7555b6c366f7d2c4fe06a3e2835cf04
lalita2407 <lalitalallu02@gmail.com>
2023-09-19 18:36:51 +0530

    changed provider version 3.22 to 3.64 and code cleanup

--- main.tf
+++ main.tf
@@ -1,56 +1,63 @@
-resource "azurerm_linux_web_app_slot" "webapp_slot" {
-    name = var.name
-    app_service_id = var.app_service_id
-    site_config {
-        app_command_line = var.app_command_line
-        ftps_state = var.ftps_state
-        dynamic "application_stack" {
-          for_each = var.current_stack == "python" ? [1] : []
-          content {
-            python_version = var.stack_version
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "node" ? [1] : []
-          content {
-            node_version = var.stack_version
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "docker" ? [1] : []
-          content {
-            docker_image     = var.stack_version
-            docker_image_tag = var.docker_image_tag
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "dotnet" ? [1] : []
-          content {
-            dotnet_version = var.stack_version
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "java" ? [1] : []
-          content {
-            java_server  = var.java_server
-            java_version = var.stack_version
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "php" ? [1] : []
-          content {
-            php_version = var.stack_version
-          }
-        }
-        dynamic "application_stack" {
-          for_each = var.current_stack == "ruby" ? [1] : []
-          content {
-            ruby_version = var.stack_version
-          }
-        }
-          
+# Creates a Windows webapp
+resource "azurerm_linux_web_app" "example" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = var.service_plan_id
 
+  site_config {
+    ftps_state       = var.ftps_state
+    app_command_line = var.app_command_line
+    dynamic "application_stack" {
+      for_each = var.current_stack == "docker" ? [1] : []
+      content {
+        docker_image_name        = var.stack_version
+        docker_registry_url      = var.docker_registry_url
+        docker_registry_username = var.docker_registry_username
+        docker_registry_password = var.docker_registry_password
+      }
     }
-
-  
+    dynamic "application_stack" {
+      for_each = var.current_stack == "dotnet" ? [1] : []
+      content {
+        dotnet_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "go" ? [1] : []
+      content {
+        go_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "java" ? [1] : []
+      content {
+        java_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "node" ? [1] : []
+      content {
+        node_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "php" ? [1] : []
+      content {
+        php_version = var.stack_version
+      }
+    }
+    dynamic "application_stack" {
+      for_each = var.current_stack == "python" ? [1] : []
+      content {
+        python_version = var.stack_version
+      }
+    }
+  }
+  dynamic "application_stack" {
+    for_each = var.current_stack == "ruby" ? [1] : []
+    content {
+      ruby_version = var.stack_version
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,42 +1,56 @@
 variable "name" {
-    description = "The name of the webapp deployment slot to be created"
-    type = string
-  
+  type        = string
+  description = "The name which should be used for this Windows Windows Web App."
 }
-variable "app_service_id" {
-    description = "The resource id of the existing webapp "
-    type = string
-  
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Resource Group where the Windows Web App should exist."
 }
-variable "current_stack" {
-    description = "The application stack of the webapp"
-    type = string
-  
+
+variable "location" {
+  type        = string
+  description = "The Azure Region where the Windows Web App should exist.Central India or South India"
 }
-variable "stack_version" {
-    description = "Application Stack Version"
-    type = string
-  
+
+variable "service_plan_id" {
+  type        = string
+  description = "The ID of the Service Plan that this Windows App Service will be created in."
+
 }
-variable "java_server" {
-    description = "The Java server type"
-    type = string
-    default = "JAVA"
-  
-}
-variable "docker_image_tag" {
-    description = "he image Tag to use. e.g. latest"
-    type = string
-    default = "latest"
-  
-}
-variable "app_command_line" {
-    description = "The App command line to launch"
-    type = string
-  
-}
+
 variable "ftps_state" {
-    description = "The State of FTP / FTPS service. Possible values include AllAllowed, FtpsOnly, and Disabled."
-    type = string
-  
+  type        = string
+  description = "The State of FTP / FTPS service. Possible values include: AllAllowed, FtpsOnly, Disabled."
+  default     = "FtpsOnly"
+}
+
+variable "app_command_line" {
+  type        = string
+  description = "The App command line to launch."
+  default     = ""
+}
+
+variable "stack_version" {
+  type        = string
+  description = "Version of the selected stack."
+
+}
+
+variable "docker_registry_url" {
+  type        = string
+  description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
+  default     = "https://mcr.microsoft.com"
+}
+variable "docker_registry_username" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "admin"
+
+}
+variable "docker_registry_password" {
+  type        = string
+  description = "The User Name to use for authentication against the registry to pull the image."
+  default     = "Welcome@1234"
+
 }
