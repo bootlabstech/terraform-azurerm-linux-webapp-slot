2d6aabdb6e3559e59a854377363cf36432c2e3b9
lalita2407 <lalitalallu02@gmail.com>
2023-10-16 15:04:24 +0530

    dummy

--- main.tf
+++ main.tf
@@ -1,63 +1,6 @@
-# Creates a Windows webapp
-resource "azurerm_linux_web_app" "example" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  service_plan_id     = var.service_plan_id
+resource "azurerm_linux_web_app_slot" "example" {
+  name           = var.name
+  app_service_id = var.app_service_id
 
-  site_config {
-    ftps_state       = var.ftps_state
-    app_command_line = var.app_command_line
-    dynamic "application_stack" {
-      for_each = var.current_stack == "docker" ? [1] : []
-      content {
-        docker_image_name        = var.stack_version
-        docker_registry_url      = var.docker_registry_url
-        docker_registry_username = var.docker_registry_username
-        docker_registry_password = var.docker_registry_password
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "dotnet" ? [1] : []
-      content {
-        dotnet_version = var.stack_version
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "go" ? [1] : []
-      content {
-        go_version = var.stack_version
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "java" ? [1] : []
-      content {
-        java_version = var.stack_version
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "node" ? [1] : []
-      content {
-        node_version = var.stack_version
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "php" ? [1] : []
-      content {
-        php_version = var.stack_version
-      }
-    }
-    dynamic "application_stack" {
-      for_each = var.current_stack == "python" ? [1] : []
-      content {
-        python_version = var.stack_version
-      }
-    }
-  }
-  dynamic "application_stack" {
-    for_each = var.current_stack == "ruby" ? [1] : []
-    content {
-      ruby_version = var.stack_version
-    }
-  }
+  site_config {}
 }
--- variables.tf
+++ variables.tf
@@ -3,57 +3,9 @@ variable "name" {
   description = "The name which should be used for this Windows Windows Web App."
 }
 
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the Resource Group where the Windows Web App should exist."
-}
-variable "current_stack" {
-  type        = string
-  description = "The name of the Resource Group where the Windows Web App should exist."
-}
-variable "location" {
-  type        = string
-  description = "The Azure Region where the Windows Web App should exist.Central India or South India"
-}
 
-variable "service_plan_id" {
+variable "app_service_id" {
   type        = string
   description = "The ID of the Service Plan that this Windows App Service will be created in."
-
-}
-
-variable "ftps_state" {
-  type        = string
-  description = "The State of FTP / FTPS service. Possible values include: AllAllowed, FtpsOnly, Disabled."
-  default     = "FtpsOnly"
-}
-
-variable "app_command_line" {
-  type        = string
-  description = "The App command line to launch."
-  default     = ""
-}
-
-variable "stack_version" {
-  type        = string
-  description = "Version of the selected stack."
-
-}
-
-variable "docker_registry_url" {
-  type        = string
-  description = "The URL of the container registry where the docker_image_name is located. e.g. https://index.docker.io or https://mcr.microsoft.com. This value is required with docker_image_name."
-  default     = "https://mcr.microsoft.com"
-}
-variable "docker_registry_username" {
-  type        = string
-  description = "The User Name to use for authentication against the registry to pull the image."
-  default     = "admin"
-
-}
-variable "docker_registry_password" {
-  type        = string
-  description = "The User Name to use for authentication against the registry to pull the image."
-  default     = "Welcome@1234"
 
 }
