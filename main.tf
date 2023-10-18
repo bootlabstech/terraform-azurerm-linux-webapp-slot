61edd2a915be6e1f01d0407c5ae3989a550f2f6d
lalita2407 <lalitalallu02@gmail.com>
2023-10-18 11:16:37 +0530

    ftps and app_command_line added

--- main.tf
+++ main.tf
@@ -2,5 +2,8 @@ resource "azurerm_linux_web_app_slot" "example" {
   name           = var.name
   app_service_id = var.app_service_id
 
-  site_config {}
+  site_config {
+    ftps_state = var.ftps_state
+  }
+
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,16 @@ variable "name" {
   type        = string
   description = "The name which should be used for this Windows Windows Web App."
 }
+variable "ftps_state" {
+  type        = string
+  description = "The name which should be used for this Windows Windows Web App."
+  default = "FtpsOnly"
+}
+variable "app_command_line" {
+  type        = string
+  description = "The name which should be used for this Windows Windows Web App."
+  default = ""
+}
 
 
 variable "app_service_id" {
